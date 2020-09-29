556fe513af32257a7e1f64bf1beea821a8e4407d
Théophile <theophile.cueille@gmail.com>
2020-09-29 19:02:34 +0200

    Sympa lui

---
+++ rubyexo_09.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+user_firstname = gets.chomp
+puts "Bonjour, c'est quoi ton nom ?"
+user_lastname = gets.chomp
+puts "Bonjour, #{user_firstname} #{user_lastname}!" 
