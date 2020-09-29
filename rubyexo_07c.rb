ef1fa98d9a90772ef9f74c51b3b80fd413882c11
Théophile <theophile.cueille@gmail.com>
2020-09-29 18:47:00 +0200

    l'exo 7 qui dure 2h30

---
+++ rubyexo_07c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+puts user_name
+
+#rubyexo_07a.rb = Demande mon prénom/me permet de repondre/me renvoie ma réponse
+#rubyexo_07b.rb = Demande mon prénom/ m'indique ou répondre avec ">"/me renvoie ma réponse
+#rubyexo_07c.rb = me permet de répondre/me renvoie ma réponse
