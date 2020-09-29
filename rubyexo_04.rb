a68266d83f34f63fae707cc7769a39eb7d2d2559
Théophile <theophile.cueille@gmail.com>
2020-09-29 17:40:16 +0200

    pffff trop facile !

---
+++ rubyexo_04.rb
@@ -0,0 +1,7 @@
+#Le terminal retourne : 
+#rubyexo_04.rb:1: unterminated string meets end of file 
+#puts "Salut, ça farte ?
+
+#Car il nous signale qu'il manque un guillemet en ligne 1 !
+puts "Salut, ça farte ?
+
